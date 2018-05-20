f69d3922090381278c5a81379cb2836ba0866abd
Angel Hernandez <ahm@Angels-MBP.home>
2018-05-20 15:24:24 -0400

    Update

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,42 @@
-import * as Rx from "rxjs/Observable";
+// import * as Rx from "rxjs/Observable";
+import { Observable} from "rxjs/Observable";
 
-console.log(Rx);
+
+let observable = Observable.create((observer: any) => {
+    try {
+        observer.next('Hello!');
+        observer.next('Hello! How are you?');
+
+        setInterval(()=>{
+            observer.next('Im good');
+        }, 2000)
+
+        // observer.complete();
+        observer.next('This will not send');
+    } catch (error) {
+        observer.error(error);
+    }
+
+});
+
+var observer = observable.subscribe(
+    (x:any) => addItem(x),
+    (error:any) => addItem(error),
+    () => addItem('completed')
+);
+
+setTimeout(() => {
+    observer.unsubscribe();
+}, 6001);
+
+
+observable.subscribe( (x: any) => {
+    console.log(x);
+})
+
+function addItem(val: any) {
+    var node = document.createElement('li');
+    var textNode = document.createTextNode(val);
+    node.appendChild(textNode);
+    document.getElementById('output').appendChild(node);
+}
